8ca60e8590e86b14c0d881205e099f4ef9c61290
Akif Heren <akhe@microsoft.com>
2021-10-13 11:38:40 -0700

    push the hello world app and the sln

---
+++ Generator/MySourceGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Generator
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    public class SyntaxReceiver : ISyntaxReceiver
+    {
+        public HashSet<TypeDeclarationSyntax> TypeDeclarationsWithAttributes { get; } = new();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {   
+            if (syntaxNode is TypeDeclarationSyntax declaration
+                && declaration.AttributeLists.Any())
+            {
+                TypeDeclarationsWithAttributes.Add(declaration);
+            }
+        }
+    }
+
+    [Generator]
+    public class MySourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            /// find the main method
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+            // build up the source code
+            string source = $@"
+using System;
+
+namespace {mainMethod.ContainingNamespace.Name}
+{{
+    public static partial class {mainMethod.ContainingType.Name}
+    {{
+        static partial void HelloFrom(string name)
+        {{
+            Console.WriteLine($""Generator says: Hi from '{{name}}'"");
+        }}
+    }}
+}}
+";
+            // add the source code to the compilation
+            context.AddSource("generatedSource", source);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+            => context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+    }
+}
---
+++ SourceGenTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SourceGenTest
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
