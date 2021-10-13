80e1cbb4d09b6e0d467f71d638d7ddf68d986b13
Denis Komissarov <dekomiss@microsoft.com>
2021-10-13 10:28:28 -0700

    Ported example code

---
+++ src/Probes/UserClass.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------
+
+namespace Hackathon21Poc.Generators.Probes
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public abstract partial class UserClass
+    {
+        protected UserClass(string probeType)
+        {
+        }
+
+        protected abstract Task ProbeImplementation();
+
+        protected Task RunAsync() {
+            this.GeneratedProbeImplementation();
+        }
+    }
+}
